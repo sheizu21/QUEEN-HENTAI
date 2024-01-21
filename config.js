94e17866c45a1c00794ed9e1cea435c82c4df55b
sheizu21 <147534090+sheizu21@users.noreply.github.com>
2024-01-21 15:33:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['994404257236', 'Sheizu studio', true],
+  ['994404257236', 'Sheizu studio', true], 
   [''] 
 ] //Number of owners
 
